e60acc62a825f0bc96bfb922626f3a8a08115655
Josh Boddy <josh@joshs-mac-pro.mynet>
2020-03-20 16:46:29 +0000

    Updated Structure for Development

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+// Initialisation of Modules and Express server
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
-const port = 9000;
+// Initialise environment variables
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-	res.send("Hello World");
+// Create a Connection to the MySQL Database
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
 });
 
-app.listen(port, () => console.log('Listening on port: ' + port));
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// An example GET request sending plain text
+app.get('/', (req, res) => {
+	res.send("Backend for ATS System");
+});
+
+// An example POST request sending json
+app.post('/test', (req, res) => {
+	let jsonResponse = {
+		"Accept": true
+	}
+	res.json(jsonResponse);
+});
+
+// Server setup to listen on a predefined port
+app.listen(process.env.PORT, () => console.log('Listening on port: ' + process.env.PORT));
