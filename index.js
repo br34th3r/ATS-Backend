3004ae4d99c30dfe96e468edabcd7b8b522d388d
br34th3r <joshuaboddy1@gmail.com>
2020-03-24 19:28:49 +0000

    Moved DB Connection to separate class

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 // Initialisation of Modules and Express server
 const express = require('express');
-const mysql = require('mysql');
 const app = express();
+const MysqlConnection = require('./classes/MysqlConnection.js');
 
 // Initialise environment variables
 require('dotenv').config()
 
 // Create a Connection to the MySQL Database
-var con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+var conn = new MysqlConnection();
+conn.init();
 
 // An example GET request sending plain text
 app.get('/', (req, res) => {
 	res.send("Backend for ATS System");
+  conn.query("SELECT * FROM blanks");
 });
 
 // An example POST request sending json
