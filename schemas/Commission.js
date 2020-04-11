a5e0979d13a30295c5470089ba2269312f377721
Josh Boddy <josh@192.168.1.205>
2020-04-11 01:58:16 +0100

    Finished All Defined Routes apart from Reports

---
+++ schemas/Commission.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Commission = new Schema({
+  amount: Number,
+  blankType: {
+    type: Number,
+    enum: [101, 201, 420, 440, 444, 451, 452],
+    default: 101
+  }
+});
+
+module.exports = mongoose.model("Commission", Commission);
