1750f2d03a6d524fa5a9176cd3382d9f3042f957
br34th3r <joshuaboddy1@gmail.com>
2020-04-12 23:39:12 +0100

    Fixed assignment route

--- components/AssignmentRoutes.js
+++ components/AssignmentRoutes.js
@@ -3,8 +3,14 @@ const Blank = require('../schemas/Blank');
 module.exports = function(app) {
   // Assign/Reassign a Blank
   app.patch('/blanks/assign/:start/:end/:agentID', (req, res) => {
-  	for (var i = parseInt(req.params.end); i <= parseInt(req.params.start); i--) {
-  		Blank.findOneAndUpdate({ number: i.toString().padStart(8, "0") }, { AgentID: req.params.agentID });
+  	for (var i = parseInt(req.params.end); i >= parseInt(req.params.start); i--) {
+  		Blank.findOne({ number: i.toString().padStart(8, "0") }, (err, doc) => {
+        if (err) throw err;
+        if (doc != null) {
+          doc.AgentID = (req.params.agentID != "NULL") ? req.params.agentID : null;
+          doc.save();
+        }
+      });
   	}
   	res.send("Assigned Blank(s) to Agent!");
   	console.log("Assigned Blank(s) to Agent!")
