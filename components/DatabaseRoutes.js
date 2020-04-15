bf3ba7375ebf687b3691166a6212c94f25cbb1de
Josh Boddy <josh@joshs-mac-pro.mynet>
2020-04-15 23:16:25 +0100

    Updated

--- components/DatabaseRoutes.js
+++ components/DatabaseRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   // Backup the Database
   app.get('/database/backup', (req, res) => {
   	var datetime = Date.now();
-  	var args = ['--db', 'ats-db', '--out', datetime ]
+  	var args = ['--db', 'ats-db', '--out', `${__dirname}/../backups/${datetime}` ]
         , mongodump = spawn('/usr/local/bin/mongodump', args);
     mongodump.stdout.on('data', function (data) {
   		console.log("Backed Up!");
@@ -16,23 +16,27 @@ module.exports = function(app) {
     mongodump.on('exit', function (code) {
       console.log('Finished');
     });
-  	res.send("Finished Backing Up!");
+  	res.status(200).json({ ok: true, backup: datetime });
   });
 
   // Restore Database from Backup
-  app.get('/database/restore/:backup', (req, res) => {
-  	var args = [ "--drop", req.params.backup + "/" ]
-        , mongodump = spawn('/usr/local/bin/mongorestore', args);
-    mongodump.stdout.on('data', function (data) {
-  		console.log("Restored!");
-    });
-    mongodump.stderr.on('data', function (data) {
-      console.log(data.toString());
-    });
-    mongodump.on('exit', function (code) {
-      console.log('Finished');
-    });
-  	res.send("Finished Restoring!");
+  app.post('/database/restore', (req, res) => {
+    if(req.body.backup) {
+    	var args = [ "--drop", `${__dirname}/../backups/${req.body.backup}/` ]
+          , mongodump = spawn('/usr/local/bin/mongorestore', args);
+      mongodump.stdout.on('data', function (data) {
+    		console.log("Restored!");
+      });
+      mongodump.stderr.on('data', function (data) {
+        console.log(data.toString());
+      });
+      mongodump.on('exit', function (code) {
+        console.log('Finished');
+      });
+    	res.status(200).json({ ok: true });
+    } else {
+      res.status(400).json({ errors: "Invalid Query! "});
+    }
   });
 
   // Add an Exchange Rate
