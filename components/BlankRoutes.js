a7ed593ee5cb9766738ed5d5880c6b82b4b3f355
Josh Boddy <josh@192.168.1.205>
2020-04-15 06:44:14 +0100

    Updated Remove Blanks for Frontend

--- components/BlankRoutes.js
+++ components/BlankRoutes.js
@@ -23,47 +23,23 @@ module.exports = function(app) {
 
   			newBlank.save();
   		}
-  		res.send("Blank(s) Added!");
+  		res.status(200).json({ ok: true });
   		console.log("Added Blank(s)!");
   	} else {
       res.send("Invalid Query!");
     }
   });
 
-  // Remove a Blank
-  app.delete('/removeBlank/:blankID', (req, res) => {
-  	Blank.findOne({ _id: req.params.blankID }, (err, doc) => {
-  		if (err) throw err;
-  		if (doc != null) {
-  			if (doc.AgentID == null) {
-  				Blank.findByIdAndRemove({_id: doc._id}, (err, offer) => {
-  					if (err) throw err;
-  					console.log("Blank Removed!");
-  					res.send("Blank Removed!");
-  				});
-  			} else {
-  				res.send("Failure, Blank is Assigned!");
-  			}
-  		}
-  	});
-  });
-
   // Bulk Remove Blanks
-  app.delete('/removeBlanks/:start/:end', (req, res) => {
-  	for (var i = parseInt(req.params.start); i <= parseInt(req.params.end); i++) {
-  		Blank.findOne({ number: i.toString().padStart(8, "0") }, (err, blank) => {
-  			if (err) throw err;
-  			if (blank != null) {
-  				if (blank.AgentID == null) {
-  					Blank.findByIdAndRemove({_id: blank._id}, (err, offer) => {
-  						if (err) throw err;
-  					});
-  				}
-  			}
-  		});
-  	}
-  	console.log("Removal Terminated!");
-  	res.send("Removal Terminated!");
+  app.delete('/removeBlanks', (req, res) => {
+	  if (req.body.start && req.body.end) {
+			Blank.deleteMany({ AgentID: null, number: { $lte: req.body.end, $gte: req.body.start } }, (err, blanks) => {
+				if (err) res.status(400).json({ errors: err });
+				res.status(200).json({ ok: true, deleted: blanks.length })
+			});
+    } else {
+      res.status(400).json({ errors: "Invalid Query!" });
+    }
   });
 
   // Get blanks by a type
