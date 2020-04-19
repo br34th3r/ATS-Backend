4548996ca2f05a8e5bb91b246367484e7cafacd1
Josh Boddy <josh@joshs-mac-pro.mynet>
2020-04-19 14:53:12 +0100

    Final

--- components/BlankRoutes.js
+++ components/BlankRoutes.js
@@ -42,39 +42,18 @@ module.exports = function(app) {
     }
   });
 
-  // Get blanks by a type
-  app.get('/blanks/type', (req, res) => {
-    if(req.body.blankType) {
-    	Blank.find({ type: req.body.type }, (err, docs) => {
-    		if (err) { res.status(400).json({ errors: err }) };
-    		res.status(200).json({ ok: true, blanks: docs });
-    		console.log("Sent Blanks of Type");
-    	});
-    } else {
-      res.status(400).json({ errors: "Invalid Query!" });
-    }
-  });
-
-  // Get a Blank by ID
-  app.get('/blanks/id', (req, res) => {
-    if (req.body.blankID) {
-      Blank.findById(req.body.blankID, (err, doc) => {
-    		if (err) throw err;
-    		res.status(200).json({ ok: true, blank: doc });
-    		console.log("Found Blank by ID");
-    	});
-    } else {
-      res.status(400).json({ errors: "Invalid Query!" });
-    }
-  });
-
-  // Get a Blank by Number
-  app.get('/blanks/number', (req, res) => {
-    if(req.body.blankNumber) {
-      Blank.find({ number: req.body.number.toString().padStart(8, "0") }, (err, doc) => {
-    		if (err) { res.status(400).json({ errors: err }) };
-    		res.status(200).json({ ok: true, blank: doc });
-    		console.log("Retrieved Blank by Number");
+  // Find a Blank by Type and Number
+  app.post('/blanks/find', (req, res) => {
+    if(req.body.blankNumber && req.body.blankType) {
+      Blank.find({ type: req.body.blankType, number: req.body.number.toString().padStart(8, "0") }, (err, doc) => {
+    		if (err) { res.status(400).json({ errors: err }); return; }
+        if (!blank) { res.status(400).json({ errors: "Blank not Found!" }); return; }
+        Ticket.findOne({ blankID: doc._id }, (err, ticket) => {
+          if (err) { res.status(400).json({ errors: err }); return; }
+          if (!ticket) { res.status(400).json({ errors: "No Ticket Matching this Blank" }); return; }
+          res.status(200).json({ ok: true, ticket: ticket });
+          console.log('Retrieved Ticket!');
+        });
     	});
     } else {
       res.status(400).json({ errors: "Invalid Query!" });
--- components/TicketRoutes.js
+++ components/TicketRoutes.js
@@ -11,63 +11,68 @@ const Sale = require('../schemas/Sale');
 module.exports = function(app) {
   // Record a sold ticket
   app.post('/addSoldTicket', (req, res) => {
-    console.log(req.body);
-  	if (req.body.saleType && req.body.paymentMethod && req.body.blankNumber && req.body.blankType && req.body.customerID && req.body.from && req.body.to && req.body.localTax && req.body.payLater && req.body.userID) {
+  	if (req.body.saleType && req.body.paymentMethod && req.body.blankNumber && req.body.blankType && req.body.customerID && req.body.from && req.body.to && req.body.localTax && req.body.payLater) {
   		ExchangeRate.findOne({}, (err, rate) => {
   			if(rate != null) {
   				Customer.findOne({ _id: req.body.customerID }, (err, customer) => {
   					if (customer != null) {
-  						Commission.findOne({ blankType: req.body.blankType }, (err, commission) => {
-  							if (commission != null) {
-  								Blank.findOne({ type: req.body.blankType, number: req.body.blankNumber.toString().padStart(8, "0") }, (err, blank) => {
-  									if (blank != null) {
-  										Payment.findOneOrCreate({
-  											type: req.body.paymentMethod,
-  										  cardNumber: req.body.cardNumber ? req.body.cardNumber : null,
-  										  expiryDate: req.body.cardExpiry ? req.body.cardExpiry : null,
-  										  cvc: req.body.cvc ? req.body.cvc : null,
-  										  issuer: req.body.cardIssuer ? req.body.cardIssuer : null,
-  										  customerID: req.body.customerID
-  										}, (err, payment) => {
-  											if (err) { res.status(400).json({ errors: err }); }
-												let newSale = new Sale({
-													saleType: req.body.saleType,
-													isPaid: !req.body.payLater,
-													costLocal: req.body.costLocal ? req.body.costLocal : null,
-													costUSD: req.body.costUSD ? req.body.costUSD : null,
-													localTaxes: req.body.localTax,
-													otherTaxes: req.body.otherTax ? req.body.otherTax : null,
-													currentRate: rate._id,
-													commission: commission._id,
-													agentID: req.body.userID,
-													paymentID: payment._id,
-													customerID: customer._id,
-													blankID: blank._id
-												});
+              Agent.findOne({ userID: req.session.user._id }, (err, agent) => {
+                if (agent != null) {
+                  Commission.findOne({ blankType: req.body.blankType }, (err, commission) => {
+      							if (commission != null) {
+      								Blank.findOne({ type: req.body.blankType, number: req.body.blankNumber.toString().padStart(8, "0") }, (err, blank) => {
+      									if (blank != null) {
+      										Payment.findOneOrCreate({
+      											type: req.body.paymentMethod,
+      										  cardNumber: req.body.cardNumber ? req.body.cardNumber : null,
+      										  expiryDate: req.body.cardExpiry ? req.body.cardExpiry : null,
+      										  cvc: req.body.cvc ? req.body.cvc : null,
+      										  issuer: req.body.cardIssuer ? req.body.cardIssuer : null,
+      										  customerID: req.body.customerID
+      										}, (err, payment) => {
+      											if (err) { res.status(400).json({ errors: err }); }
+    												let newSale = new Sale({
+    													saleType: req.body.saleType,
+    													isPaid: !req.body.payLater,
+    													costLocal: req.body.costLocal ? req.body.costLocal : null,
+    													costUSD: req.body.costUSD ? req.body.costUSD : null,
+    													localTaxes: req.body.localTax,
+    													otherTaxes: req.body.otherTax ? req.body.otherTax : null,
+    													currentRate: rate._id,
+    													commission: commission._id,
+    													agentID: req.session..userID,
+    													paymentID: payment._id,
+    													customerID: customer._id,
+    													blankID: blank._id
+    												});
 
-                        let newTicket = new Ticket({
-                          isValid: true,
-                          departure: req.body.from,
-                          destination: req.body.to,
-                          saleDate: Date.now(),
-                          blankID: blank._id,
-                          customerID: customer._id
-                        })
+                            let newTicket = new Ticket({
+                              isValid: true,
+                              departure: req.body.from,
+                              destination: req.body.to,
+                              saleDate: Date.now(),
+                              blankID: blank._id,
+                              customerID: customer._id
+                            })
 
-												newSale.save();
-                        newTicket.save();
-												res.status(200).json({ ok: true });
-  										});
+    												newSale.save();
+                            newTicket.save();
+    												res.status(200).json({ ok: true });
+      										});
 
-  										blank.sold = true;
-  										blank.save();
-  									} else {
-  										res.status(400).json({ errors: "Error, Blank not Found!" });
-  									}
-  								});
-  							} else {
-  								res.status(400).json({ errors: "Error, Commission not Found!" });
-  							}
+      										blank.sold = true;
+      										blank.save();
+      									} else {
+      										res.status(400).json({ errors: "Error, Blank not Found!" });
+      									}
+      								});
+      							} else {
+      								res.status(400).json({ errors: "Error, Commission not Found!" });
+      							}
+                  });
+                } else {
+                  res.status(400).json({ errors: "Agent ID Not Found!" });
+                }
   						});
   					} else {
   						res.status(400).json({ errors: "Error, Customer not Found!" });
