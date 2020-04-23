0a50b811caaeab3aa9d5162cbcc22ded7c067175
Kamila <kamilamocek@gmail.com>
2020-04-23 22:45:29 +0200

    Added blank number to the request

--- components/AssignmentRoutes.js
+++ components/AssignmentRoutes.js
@@ -2,10 +2,11 @@ const Blank = require('../schemas/Blank'); // a variable is created that loads a
 
 module.exports = function(app) {
   // Assign/Reassign a Blank
-  app.patch('/blanks/assign', (req, res) => {
-    if(req.body.start && req.body.end && req.body.agentID) {
-      Blank.updateMany({ number: {$lte: req.body.end, $gte: req.body.start }}, { AgentID: req.body.agentID }, (err, doc) => {
+  app.post('/blanks/assign', (req, res) => {
+    if(req.body.blankType && req.body.agentID && req.body.blankNumber) {
+      Blank.updateMany({ type: req.body.blankType, number: req.body.blankNumber }, { AgentID: req.body.agentID }, (err, doc) => {
         if (err) { res.status(400).json({ errors: err }); }
+        res.status(200).json({ ok: true });
       });
     } else {
       res.status(400).json({ errors: "Invalid Query!" });
